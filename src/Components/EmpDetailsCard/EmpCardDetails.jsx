6f5d55c6d1dcac5521f5c7798ac4e13ada90cee0
Dayakar181 <bhagavandas.kommu@gmail.com>
2022-11-28 08:34:29 +0530

    Added: Router

--- src/Components/EmpDetailsCard/EmpCardDetails.jsx
+++ src/Components/EmpDetailsCard/EmpCardDetails.jsx
@@ -1,4 +1,4 @@
-import React,  {useEffect, useState, href, useNavigate} from 'react';
+import React,  {useEffect, useState, href, useNavigate, searchData} from 'react';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -9,31 +9,24 @@ import Paper from '@mui/material/Paper';
 import Toolbar from '@mui/material/Toolbar';
 import './EmpCardDetails.css';
 import Card from '@mui/material/Card';
-import Input from '@mui/material/Input';
+import Button from '@mui/material/Button';
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
 
 
 import axios from 'axios';
-import { InputBase } from '@mui/material';
+import { TextField } from '@mui/material';
 
 export default function EmpCardDetails()  {
     const [employee, setEmployee] = useState([])
     const [shouldrefresh, addrefresh] = useState(false)
+    const [searchData, setSearchData] = useState([]);
 
     useEffect(() => {
         loadEmployee();
     }, [shouldrefresh]);
 
-    // const loadEmployee = async () => {
-    //     const result = await axios.get("http://localhost:8080/getAllEmployees")
-    //     console.log("not working...."+ result.data.obj);
-
-    //     setEmployee(result.data.obj);
-        
-    //      console.log(employee[0]);
-    // }
-         
+  
     const loadEmployee = async () => {
         const result = await axios.get("http://localhost:8080/getAllEmployees")
         setEmployee(result.data.obj);
@@ -42,7 +35,7 @@ export default function EmpCardDetails()  {
     }
 
     async function deletearr(props){
-        const result = await axios.delete("http://localhost:8080/deleteEmployee?employeeName="+ props)
+        const result = await axios.delete("http://localhost:8080/deleteEmployee?employeeId="+ props)
         alert("Emplyee Deleted")
         console.log("delete",result.data)
         addrefresh(true)
@@ -59,8 +52,27 @@ export default function EmpCardDetails()  {
         const result = await axios.put("http://localhost:8080/updateEmployee",props)
         alert("Emplyee Updated")
         console.log("Update",result.data)
+        // localStorage.setItem('employeeName', result.data.obj);
+        // const textFromStorage = localStorage.getItem('employeeName');
+        // console.log(textFromStorage);
 
     } 
+
+    const handleEmpSearch = async () => {
+        console.log(searchData)
+        const result = await axios.get(`http://localhost:8080/getsingleemployee/${searchData}`)
+            .then((response) => {
+                console.log(response.data.obj);
+            })
+            .catch((error) => {
+                console.log(error.response.data.message)
+            });
+        setEmployee(result.data.obj);
+
+    }
+
+
+   
 
 //     let navigate = useNavigate(); 
 //   const routeChange = () =>{ 
@@ -74,17 +86,17 @@ export default function EmpCardDetails()  {
                 <Toolbar></Toolbar>
                 <Card className = 'form'>
                 
-                    <Input variant="outlined" 
-                    placeholder="Search..."
-                    className='search'
-                   
-                    />
+                <TextField placeholder='search ' value={searchData}
+                    onChange={(e) => setSearchData(e.target.value)}/>
+
+                <Button onClick={handleEmpSearch}>Search</Button>
                     <h3 className='title'>Employee Details
                     <button className='user'  placeholder='+Add User'><a href="http://localhost:3000/Homepage" target="_blank" rel="noreferrer">+AddUser</a></button></h3>
                 <TableContainer component={Paper}>
                     <Table sx={{ minWidth: 650 }} aria-label="simple table">
                         <TableHead>
                             <TableRow className='TableRow' style={{backgroundColor: '#42515F', width: '1549px',height: '67px'}}>
+                            <TableCell className='TableHeaderCell'>ID</TableCell>
                                 <TableCell className='TableHeaderCell'>Name</TableCell>
                                 <TableCell className='TableHeaderCell'>Gender</TableCell>
                                 <TableCell className='TableHeaderCell'>Department</TableCell>
@@ -99,7 +111,7 @@ export default function EmpCardDetails()  {
                                       employee.map((employees, index)=>(
                                 
                             <TableRow key={index}>   
-                                    
+                                         <TableCell> {employees.employeeId} </TableCell>
                                         <TableCell> {employees.employeeName} </TableCell>
                                         <TableCell> {employees.gender} </TableCell>
                                         <TableCell> {employees.department}</TableCell>
@@ -112,7 +124,7 @@ export default function EmpCardDetails()  {
                                     }}>  <EditIcon/>  </TableCell> 
                                     {/* </a> */}
                                     <TableCell onClick={()=>{
-                                        deletearr(employees.employeeName)
+                                        deletearr(employees.employeeId)
                                     }}><DeleteIcon/> </TableCell>
 
                             </TableRow>
